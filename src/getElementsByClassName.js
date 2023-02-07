e4d4e734dc7e33269cf306bd885ad61b575f7aa4
Mason Lang <masonisblue@gmail.com>
2023-02-06 17:01:32 -0800

    Passed bare minimum requirements in first two recursion review questions

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,60 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, node
 ) {
-  // your code here
+
+  // create nodelist variable
+  var nodeList = document.body;
+  // create result variable
+  var result = [];
+  // initiate it with document.body
+  var search = function (node) {
+  // if current node has classname
+    console.log('node', node);
+    if (node.classList && node.classList.contains(className)) {
+    // push node into result variable
+      console.log('node2', node);
+      result.push(node);
+      console.log('result', result);
+    }
+    // if current node has children
+    for (var i = 0; i < node.childNodes.length; i++) {
+      search(node.childNodes[i]);
+    }
+  // return result
+  // loop through each child node and recursively call function
+  };
+  search(document.body);
+  // return result
+  return result;
+
 };
+
+/*
+// your code here
+
+  // create nodelist variable
+  var nodeList = document.body;
+  // create result variable
+  var result = [];
+  // initiate it with document.body
+  var search = function (node) {
+  // if current node has classname
+    console.log('node', node);
+    if (node.classList && node.classList.contains(className)) {
+    // push node into result variable
+    console.log('node2', node);
+      result.push(node);
+      console.log('result', result);
+    }
+    // if current node has children
+    for (var i = 0; i < node.childNodes.length; i++) {
+      search(node.childNodes[i]);
+    }
+  // return result
+  // loop through each child node and recursively call function
+  };
+  search(document.body);
+
+*/
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,72 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // if obj is undefined, return 'null'
+  if (obj === undefined) {
+    return undefined;
+  }
+
+  // if obj is a function, return undefined
+  if (typeof obj === 'function') {
+    return undefined;
+  }
+  // if obj is null, return 'null';
+  if (obj === null) {
+    return 'null';
+  }
+  // if obj is a string, return ' + "string" + '
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // if obj is a boolean, return string version of boolean
+  if (typeof obj === 'boolean') {
+    return '' + obj;
+  }
+  // if obj is a number, return string version of number
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+  // if obj is an array,
+  if (Array.isArray(obj)) {
+    //if length is 0, return []
+    if (obj.length === 0) {
+      return '[]';
+    }
+    var result = [];
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+
+    return '[' + result + ']';
+  }
+  // if obj is an object,
+  if (typeof obj === 'object') {
+    // if object is an empty object
+    console.log(obj, 'obj');
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        delete obj[key];
+      }
+    }
+    var result = '';
+    for (var i = 0; i < Object.keys(obj).length; i++) {
+      // push stringified key into result
+      var key = Object.keys(obj)[i];
+
+      result += stringifyJSON(key);
+      result += ':' + stringifyJSON(obj[key]);
+
+      if (i !== Object.keys(obj).length - 1) {
+        result += ',';
+      }
+
+      console.log('result', result);
+      // push stringified value into result
+    }
+
+    return '{' + result + '}';
+  }
 };
